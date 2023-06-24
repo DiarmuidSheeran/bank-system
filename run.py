0e56b485a46369ff784c0fa9e3c45f7cc1de2df9
Diarmuid Sheeran <dssheeran@outlook.com>
2023-06-24 13:54:25 +0000

    Create User Class

--- run.py
+++ run.py
@@ -3,10 +3,24 @@ import re
 import os
 
 def clear():
-    # check if operating system is mac and linux or windows.
+    """ 
+    check if operating system is mac and linux or windows.
+    """
     if os.name == 'posix':
         _ = os.system('clear')
 
     else:
         # else operating system is windows
         _ = os.system('cls')
+
+class User:
+    """
+    Create an object for a user to be created with different properties
+    """
+    def __init__(self, fname, lname, age, country, email, account_num): 
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+        self.country = country
+        self.email = email
+        self.account_num = account_num
