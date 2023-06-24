1e94e8ef77d5ebe264ae14868a477c281ed58d51
Diarmuid Sheeran <dssheeran@outlook.com>
2023-06-24 13:52:01 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import re
+import os
+
+def clear():
+    # check if operating system is mac and linux or windows.
+    if os.name == 'posix':
+        _ = os.system('clear')
+
+    else:
+        # else operating system is windows
+        _ = os.system('cls')
